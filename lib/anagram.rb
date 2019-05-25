d4b073f0bd271e9c3a0f60015aaaab65aba39c8a
Chris Kay <chriskay25@gmail.com>
2019-05-25 02:04:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,14 +12,10 @@ class Anagram
     split_array.map {|w| w == split_word ? w : nil}.reject(&:nil?).join
   end 
   
-   def match(array)
+  def match(array)
     split_word = @word.split("").sort
     split_array = array.collect {|e| e.split("").sort}.select {|w| w == split_word}.flatten
-    array.map.with_index do |el, ind|
-      if el.split("").sort == split_array
-        return el
-      end 
-    end 
+    array.select {|el| el.split("").sort == split_array}
   end 
   
 end 
