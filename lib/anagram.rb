fa0b13d2ba7d755ca7403036ba402e05ec8704ac
Chris Kay <chriskay25@gmail.com>
2019-05-25 01:01:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    split_word = @word.split("")
+  end 
+  
+end 
